1ac98dc558a0f31141bb8fe7f6f6e6741c92ac32
Rui Almeida <ruialmeida198@gmail.com>
2024-05-08 10:23:56 +0100

    Fix icon and text sizes

--- app/src/main/java/com/eyther/lumbridge/features/profile/overview/screens/ProfileOverviewScreen.kt
+++ app/src/main/java/com/eyther/lumbridge/features/profile/overview/screens/ProfileOverviewScreen.kt
@@ -2,6 +2,7 @@ package com.eyther.lumbridge.features.profile.overview.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,18 +13,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -144,27 +148,32 @@ private fun ProfileHeader(
                 state.email?.let {
                     Text(
                         text = it,
-                        style = runescapeTypography.bodyLarge
+                        style = runescapeTypography.bodyMedium
                     )
                 }
 
                 Text(
                     text = "From ${state.locale?.name?.capitalise() ?: "Unknown"}",
-                    style = runescapeTypography.bodyLarge
+                    style = runescapeTypography.bodyMedium
                 )
             }
 
             Spacer(modifier = Modifier.weight(1f))
 
             Icon(
-                modifier = Modifier.clickable {
-                    onNavigate(
-                        ProfileNavigationItem.EditProfile,
-                        navController
-                    )
-                },
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = rememberRipple(bounded = false)
+                    ) {
+                        onNavigate(
+                            ProfileNavigationItem.EditProfile,
+                            navController
+                        )
+                    },
                 painter = painterResource(id = R.drawable.ic_edit),
-                contentDescription = "Edit profile"
+                contentDescription = "Edit Profile"
             )
         }
     }
