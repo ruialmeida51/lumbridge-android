3019cce596df7065bb6df16b2af9d5183041fcde
Rui Almeida <ruialmeida198@gmail.com>
2024-05-08 18:13:08 +0100

    increment versionc ode

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -5,7 +5,7 @@ object Config {
 
     const val MIN_SDK = 31
     const val TARGET_SDK = 34
-    const val VERSION = 1
+    const val VERSION = 2
     const val VERSION_NAME = "1.2"
 
     const val JAVA_VERSION = "17"
