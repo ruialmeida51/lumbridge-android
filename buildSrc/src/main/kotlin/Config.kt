19430e248657a9107c6792e846e62729091a86cf
Rui Almeida <ruialmeida198@gmail.com>
2024-10-22 15:42:48 +0100

    Update Config.kt

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,8 +6,8 @@ object Config {
     const val MIN_SDK = 29
     const val TARGET_SDK = 34
 
-    const val VERSION = 28
-    const val VERSION_NAME = "1.7.11"
+    const val VERSION = 29
+    const val VERSION_NAME = "1.8.0"
 
     const val JAVA_VERSION = "17"
     val JAVA_TARGET = JavaVersion.VERSION_17
