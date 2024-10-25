5345581e1153ed942093eb3883ad27b8cb486ac5
Rui Almeida <ruialmeida198@gmail.com>
2024-10-25 13:25:23 +0100

    version & build error

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -6,8 +6,8 @@ object Config {
     const val MIN_SDK = 29
     const val TARGET_SDK = 34
 
-    const val VERSION = 30
-    const val VERSION_NAME = "1.8.1"
+    const val VERSION = 31
+    const val VERSION_NAME = "1.9.0"
 
     const val JAVA_VERSION = "17"
     val JAVA_TARGET = JavaVersion.VERSION_17
