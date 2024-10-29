a003bc32b442cabe81486f6aea0dadde0b503371
Rui Almeida <ruialmeida198@gmail.com>
2024-10-29 17:59:24 +0000

    Update ExpensesAddScreenInputState.kt

--- app/src/main/java/com/eyther/lumbridge/features/expenses/model/add/ExpensesAddScreenInputState.kt
+++ app/src/main/java/com/eyther/lumbridge/features/expenses/model/add/ExpensesAddScreenInputState.kt
@@ -1,6 +1,5 @@
 package com.eyther.lumbridge.features.expenses.model.add
 
-import com.eyther.lumbridge.domain.model.expenses.ExpensesCategoryTypes
 import com.eyther.lumbridge.model.expenses.ExpensesCategoryTypesUi
 import com.eyther.lumbridge.ui.common.composables.model.input.DateInputState
 import com.eyther.lumbridge.ui.common.composables.model.input.TextInputState
